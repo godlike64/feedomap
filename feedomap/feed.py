f80a10e45b6f904943fcda785925939fadc1eaa0
godlike <godlike64@gmail.com>
2014-09-23 12:20:36 -0300

    Corrected __eq__ for FeedEntry once more

--- feedomap/feed.py
+++ feedomap/feed.py
@@ -72,10 +72,10 @@ class FeedEntry(object):
                 self.summary = entry['summary']
             except KeyError:
                 self.summary = ''
-        self.plain_summary = striphtml(self.summary)
     
     def __str__(self):
         return str(self.__dict__)
 
     def __eq__(self, other): 
-        return self.title == other.title and self.link == other.link and self.plain_summary == other.plain_summary
+        return self.title == other.title and self.link == other.link and \
+            striphtml(self.summary) == striphtml(other.summary)
